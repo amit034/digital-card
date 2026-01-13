23a045c59b7942538ce85650b3d41abc75d0ee25
amitr_perion <amitr@perion.com>
2026-01-14 00:32:00 +0200

    Redirect root to yael-amor card instead of team list

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import { DynamicCard, HomePage } from './App.jsx'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { DynamicCard } from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Navigate to="/yael-amor" replace />} />
         <Route path="/:slug" element={<DynamicCard />} />
       </Routes>
     </HashRouter>
