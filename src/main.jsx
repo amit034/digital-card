17c0b6a13e01b736c026b1cd9b84af7cacdcf8aa
amitr_perion <amitr@perion.com>
2026-01-15 19:42:19 +0200

    Add HashCleanup component to remove redundant hash on /cards/ paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,37 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import { DynamicCard, CardView } from './App.jsx'
 import { getCard, defaultCard } from './cards'
 import './index.css'
 
+// Check if we're on a /cards/{slug}/ path
+const getSlugFromPath = () => {
+  const pathMatch = window.location.pathname.match(/\/cards\/([^/]+)\/?/)
+  return pathMatch ? pathMatch[1] : null
+}
+
+// Component to clean up redundant hash on /cards/ paths
+function HashCleanup({ children }) {
+  const location = useLocation()
+  
+  useEffect(() => {
+    const slugFromPath = getSlugFromPath()
+    // If we're on /cards/{slug}/ and there's a hash, remove it
+    if (slugFromPath && window.location.hash) {
+      window.history.replaceState(null, '', window.location.pathname)
+    }
+  }, [location])
+  
+  return children
+}
+
 // Smart home component - detects if we're on /cards/{slug}/ path
 function SmartHome() {
-  // Check if URL path contains /cards/{slug}/
-  const pathMatch = window.location.pathname.match(/\/cards\/([^/]+)\/?/)
+  const slugFromPath = getSlugFromPath()
   
-  if (pathMatch) {
-    const slug = pathMatch[1]
-    const cardData = getCard(slug)
+  if (slugFromPath) {
+    const cardData = getCard(slugFromPath)
     if (cardData) {
       // Render the card directly without redirect
       return <CardView cardData={cardData} />
@@ -26,10 +45,12 @@ function SmartHome() {
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<SmartHome />} />
-        <Route path="/:slug" element={<DynamicCard />} />
-      </Routes>
+      <HashCleanup>
+        <Routes>
+          <Route path="/" element={<SmartHome />} />
+          <Route path="/:slug" element={<DynamicCard />} />
+        </Routes>
+      </HashCleanup>
     </HashRouter>
   </StrictMode>,
 )
